00c0ee7781432a12cda4b355d7651e57a752e0d3
[LonneWW] <[therealsamuelbaz@gmail.com]>
2025-06-28 08:53:39 +0200

    Added DB_PORT ambient variable

--- config/database.js
+++ config/database.js
@@ -3,10 +3,11 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST, // ad esempio: "mysql-tuo-servizio"
-  user: process.env.DB_USER, // definito al momento della creazione del DB su Render
-  password: process.env.DB_PASSWORD, // come configurato su Render
-  database: process.env.DB_NAME, // il nome del database
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   multipleStatements: true,
 });
 
